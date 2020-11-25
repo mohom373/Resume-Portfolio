753f0b5fe695f00d427b16733783691eecd507fd
Mohammad <mohom373@student.liu.se>
2020-11-25 21:29:57 +0100

    Change some ts rules

--- components/layout.tsx
+++ components/layout.tsx
@@ -25,6 +25,10 @@ export default function Layout({
         />
         <meta name="og:title" content={siteTitle} />
         <meta name="twitter:card" content="summary_large_image" />
+        <meta 
+          name="viewport" 
+          content="minimum-scale=1, initial-scale=1, width=device-width"
+        />
       </Head>
       <header className={styles.header}>
         {home ? (
